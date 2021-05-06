772fbcf0c99bf8a39132207c415dbec6399ca7ee
Sudipto Roy <sudipto@ticonsys.com>
2021-05-06 20:04:06 +0600

    Initial Commit

---
+++ BrainStation-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrainStation-Test
+//
+//  Created by Sudipto Roy on 5/6/21.
+//  Copyright © 2021 Code_X Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BrainStation-TestTests/BrainStation_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  BrainStation_TestTests.swift
+//  BrainStation-TestTests
+//
+//  Created by Sudipto Roy on 5/6/21.
+//  Copyright © 2021 Code_X Systems. All rights reserved.
+//
+
+import XCTest
+@testable import BrainStation_Test
+
+class BrainStation_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
